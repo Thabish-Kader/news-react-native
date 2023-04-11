f75a793afb38b65c38c1cb06f4ab5c5eda281cb2
Thabish <kadertabish2@gmail.com>
2023-04-11 10:38:27 +0400

    assets
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { useState } from "react";
+
+const Home = () => {
+	return (
+		<View>
+			<Text>Home</Text>
+		</View>
+	);
+};
+
+export default Home;
