f04dc54186c11fd55350a9fc021db5c1f555b2f3
Thabish <kadertabish2@gmail.com>
2023-04-11 11:38:30 +0400

    header
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,42 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, SIZES, icons } from "../constants";
+import {
+	Nearbyjobs,
+	Popularjobs,
+	ScreenHeaderBtn,
+	Welcome,
+} from "../components";
 
 const Home = () => {
+	const router = useRouter();
+
 	return (
-		<View>
-			<Text>Home</Text>
-		</View>
+		<SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+			<Stack.Screen
+				options={{
+					headerStyle: { backgroundColor: COLORS.lightWhite },
+					headerShadowVisible: false,
+					headerLeft: () => (
+						<ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+					),
+					headerRight: () => (
+						<ScreenHeaderBtn
+							iconUrl={icons.profile}
+							dimension="100%"
+						/>
+					),
+				}}
+			/>
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<View style={{ flex: 1, padding: SIZES.medium }}>
+					<Welcome />
+					<Popularjobs />
+					<Nearbyjobs />
+				</View>
+			</ScrollView>
+		</SafeAreaView>
 	);
 };
 
