46eae0613de114f974eb5772ac5ef753ee1cb311
Thabish <kadertabish2@gmail.com>
2023-04-14 10:45:25 +0400

    endpoint
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- hook/useFetch.ts
+++ hook/useFetch.ts
@@ -1,8 +1,42 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useFetch = () => {
+const useFetch = (endpoint: string, query: Object) => {
 	const [data, setData] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
+
+	const options = {
+		method: "GET",
+		url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+		headers: {
+			"X-RapidAPI-Key": `${process.env.RAPID_API_KEY}`,
+			"X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+		},
+		params: {
+			query: { ...query },
+		},
+	};
+
+	const fetchData = async () => {
+		setIsLoading(true);
+		try {
+			const response = await axios.request(options);
+			setData(response.data.data);
+		} catch (error) {
+			setError(error);
+			alert("There is an error");
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const refetch = () => {
+		setIsLoading(true);
+		fetchData();
+	};
 };
